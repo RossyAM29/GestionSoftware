8c04f3533dee833efb5e547d7c6389d18753507c
Rossy A. M <102334251+RossyAM29@users.noreply.github.com>
2022-03-24 21:02:42 -0600

    Add files via upload

---
+++ PROG_TEMAS5_RAMVer2/PROG_TEMAS5_RAM/FRMCONF.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROG_TEMAS5_RAM
+{
+    public partial class FRMCONF : Form
+    {
+        public FRMCONF(int ntema)
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex= ntema;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tema = comboBox1.SelectedItem.ToString();
+            //ACTUALIZA EL TEMA EN LA CONFIGURACION
+            Form1.mus.Tema = comboBox1.SelectedIndex;
+            Form1.AplicarTema(tema);
+            
+            
+        }
+    }
+}
